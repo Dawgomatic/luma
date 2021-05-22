24548a81194e26c7309e576fbae5dc3b5b7b1880
Jeremy Ruroede <jeremyruroede@gmail.com>
2021-05-22 16:13:51 -0400

    Update and rename cmd.py to voice.py

---
+++ voice.py
@@ -0,0 +1,3 @@
+    picovoice_demo_mic \
+    --keyword_path resources/porcupine/resources/keyword_files/raspberry-pi/porcupine_raspberry-pi.ppn \
+    --context_path resources/rhino/resources/contexts/raspberry-pi/smart_lighting_raspberry-pi.rhn
