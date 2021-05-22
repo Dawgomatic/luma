e4562f8e4fc02c17db06844005377924ea501f11
Jeremy Ruroede <jeremyruroede@gmail.com>
2021-05-22 16:15:14 -0400

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,3 +1,10 @@
-    picovoice_demo_mic \
+import os
+
+def install_function():   
+    
+    
+    os.system('picovoice_demo_mic \
     --keyword_path resources/porcupine/resources/keyword_files/raspberry-pi/porcupine_raspberry-pi.ppn \
-    --context_path resources/rhino/resources/contexts/raspberry-pi/smart_lighting_raspberry-pi.rhn
+    --context_path resources/rhino/resources/contexts/raspberry-pi/smart_lighting_raspberry-pi.rhn')
+      
+install_function()
